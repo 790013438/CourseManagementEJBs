51b58e31c8459ed23ee33ebcbe0e55e0399086f1
790013438 <790013438@qq.com>
2017-09-07 21:07:47 +0800

    获取数据并传递material deliver aim
    data transfer object

--- ejbModule/snippets/ejb/CourseBean.java
+++ ejbModule/snippets/ejb/CourseBean.java
@@ -1,7 +1,15 @@
 package snippets.ejb;
 
+import java.util.List;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import snippets.dto.CourseDTO;
+import snippets.jpa.Course;
 
 /**
  * Session Bean implementation class CourseBean
@@ -18,6 +26,31 @@ public class CourseBean implements CourseBeanRemote {
      */
     public CourseBean() {
         // TODO Auto-generated constructor stub
+    }
+
+    public List<Course> getCourseEntities() {
+        //Use named query created in Course entity using @NameQuery annotation.
+        TypedQuery<Course> courseTypedQuery = entityManager.createNamedQuery("Course.findAll", Course.class);
+        return courseTypedQuery.getResultList();
+    }
+
+    @Override
+    public List<CourseDTO> getCourses() {
+        //get course entities first
+        List<Course> courseEntities = getCourseEntities();
+
+        //Create list of course DTOs. This is the result we will return
+        List<CourseDTO> courses = new ArrayList<CourseDTO>();
+
+        for (Course courseEntity : courseEntities) {
+            //Create CourseDTO from Course entity
+            CourseDTO courseDTO = new CourseDTO();
+            courseDTO.setId(courseEntity.getId());
+            courseDTO.setName(courseEntity.getName());
+            courseDTO.setCredits(course.getCredits());
+            courses.add(course);
+        }
+        return courses;
     }
 
 }
